c5dae182690a48ad02b4b59fab3550d5e7c3a082
@bandcwalsek <cwalsek@verizon.net>
2016-08-18 15:58:23 -0400

    reorganized controllers, added message posting (in progress)

--- day14/SlackCloneApp.js
+++ day14/SlackCloneApp.js
@@ -1,14 +1,13 @@
-
 /**
  * Module dependencies.
  */
 //var db = require('./tweets.js')
-var express = require('express')
- // , routes = require('./routes')
- // , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , slackdb = require('./SlackDB.js');
+var express = require('express'),
+    // , routes = require('./routes')
+    // , user = require('./routes/user')
+    http = require('http'),
+    path = require('path'),
+    slackdb = require('./SlackDB.js');
 
 
 
@@ -20,6 +19,10 @@ var db = new sqlite3.Database(filename);
 
 var app = express();
 app.use(express.static('html'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // all environments
 app.set('port', process.env.PORT || 8080);
 //app.set('views', __dirname + '/views');
@@ -45,61 +48,81 @@ app.get('/', getDefaultIndex);
 
 app.get('/channel/:userName', getChannelByUser);
 app.get('/channelChats/:channelName', getChannelChatByChannelName);
+
+
+app.post('/message/', function (req, res) {
+	var message = req.body.message;
+	var channel = req.body.channel;
+	var user = req.body.user;
+	
+	res.send('done');
+	
+});
 //app.get('/tweet/:userid', followedTweet.tweet);
 
 //app.get('/followedtweet', getFollowedTweets('abu'));
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-function getChannelByUser(req, res){
-	var str = '<B>***getChannelByUser**</B>';
-	var username = req.params.userName;
-//	var tweets =  getFollowedTweets(req.params.userid );
-	// res.send(str+ getFollowedTweets(req.params.userid ).toString());
-	console.log ('in channelJSONPromisePublic, username='+username);
-	
-	
-	res.cookie("userName", username) ;
-	slackdb.channelJSONPromisePublic(db, username).then( //done);
-			function (val) {
-				console.log('****channelJSONPromisePublic, val='+val +'*');	
-				
-		    	res.send (val);
-								
-			},
-			function (err) {
-				done(err);
-			}
-			
-		 
-	)};
-	
-	
-	function getChannelChatByChannelName (req, res){
-		var str = '<B>***getChannelChatByChannelName**</B>';
-		var channelName = req.params.channelName;
-//		var tweets =  getFollowedTweets(req.params.userid );
-		// res.send(str+ getFollowedTweets(req.params.userid ).toString());
-		console.log ('in getChannelChatByChannelName, channelName='+channelName);
-		slackdb.channelChatJSONPromisePublic(db, channelName).then( //done);
-				function (val) {
-					console.log('****channelChatJSONPromisePublic, val '+val +'*');	
-					//json
-					res.send (val);
-									
-				},
-				function (err) {
-					done(err);
-				}
-				
-			 
-		)};
+function getChannelByUser(req, res) {
+    var str = '<B>***getChannelByUser**</B>';
+    var username = req.params.userName;
+    //	var tweets =  getFollowedTweets(req.params.userid );
+    // res.send(str+ getFollowedTweets(req.params.userid ).toString());
+    console.log('in channelJSONPromisePublic, username=' + username);
 
-	function getDefaultIndex(req, res){
-		console.log ('in default.');
-		var _dirname = 'C:\\project\\ssa4_week_training\\day8\\slackclone\\html';
-		res.sendFile( _dirname+ '\\Welcome.html');
-	}
+
+    res.cookie("userName", username);
+    slackdb.channelJSONPromisePublic(db, username).then( //done);
+        function(val) {
+            console.log('****channelJSONPromisePublic, val=' + val + '*');
+
+            res.send(val);
+
+        },
+        function(err) {
+            done(err);
+        }
+
+
+    )
+};
+
+
+function getChannelChatByChannelName(req, res) {
+    var str = '<B>***getChannelChatByChannelName**</B>';
+    var channelName = req.params.channelName;
+    //		var tweets =  getFollowedTweets(req.params.userid );
+    // res.send(str+ getFollowedTweets(req.params.userid ).toString());
+    console.log('in getChannelChatByChannelName, channelName=' + channelName);
+    slackdb.channelChatJSONPromisePublic(db, channelName).then( //done);
+        function(val) {
+            console.log('****channelChatJSONPromisePublic, val ' + val + '*');
+            //json
+            res.send(val);
+
+        },
+        function(err) {
+            done(err);
+        }
+
+
+    )
+};
+
+function getDefaultIndex(req, res) {
+    console.log('in default.');
+    var _dirname = 'C:\\project\\ssa4_week_training\\day8\\slackclone\\html';
+    res.sendFile(_dirname + '\\Welcome.html');
+}
+
+
+
+function sendMessage(req, res) {
+    console.log('entering SlackCloneApp.js sendMessage(req, res)');
+    //var _dirname = 'C:\\project\\ssa4_week_training\\day8\\slackclone\\html';
+    //res.sendFile( _dirname+ '\\Welcome.html');
+}
--- day14/html/js/channel.js
+++ day14/html/js/channel.js
@@ -1,100 +1,118 @@
- 
-      var menuApp = angular.module('menuApp', ['ngRoute']);
-      var count =1;
-	  var myChannelList =[];
-      menuApp.config(function($routeProvider) {
-        $routeProvider.
-		  when('/', {                                            
-              templateUrl: "menu.html",                                               
-              controller:'MenuCtrl'  
-             }).
-		when('/:userName', {                                              
-		      templateUrl: "all.html",                                               
-              controller:'MenuCtrl'  
-             }).
-		   when('/channelChats/:channelName', {
-		   templateUrl: "all.html",
-           controller: 'ChatMessageCtrl'
-          }).
-		   otherwise({
-            redirectTo: '/'
-          })
-		  ;
-      });
+var menuApp = angular.module('menuApp', ['ngRoute']);
+var count = 1;
+var myChannelList = [];
+menuApp.config(function($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: "menu.html",
+        controller: 'MenuCtrl'
+    }).
+    when('/:userName', {
+        templateUrl: "all.html",
+        controller: 'MenuCtrl'
+    }).
+    when('/channelChats/:channelName', {
+        templateUrl: "all.html",
+        controller: 'ChatMessageCtrl'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+});
 
-	  menuApp.factory('channels', function($http){
-        return {
-          list: function (userName, callback){
+
+menuApp.factory('channels', function($http) {
+    return {
+        list: function(userName, callback) {
             $http({
-              method: 'GET',
-              url:    'http://localhost:8080/channel/'+ userName,
-              cache: true
+                method: 'GET',
+                url: '/channel/' + userName,
+                cache: true
             }).success(callback);
-          }
-        };
-      });
-	  
-     
-      var getMenu = function ($scope, channels, $routeParams){
-	  count = count+1;
-    	    console.log ( count + ', in getMenu function, userName=' + $routeParams.userName);
-			if (myChannelList.length === 0) {
-				channels.list($routeParams.userName, function(channels) {
-				  $scope.channels = channels;
-				  console.log ("***$scope.channels, " + $scope.channels);
-				  myChannelList = $scope.channels;
-				//  $scope.subviewChannelChats= "channelChats.html";
-				}
-            );
-			}	
-				
-          }
-		  
-      menuApp.controller('MenuCtrl', getMenu);
-      
-        
-      menuApp.factory('channelChats', function($http){
-  
-       //  if ( ! (typeof $routeParams.channelName === "undefined") ) {
-        // var cachedData;
-  //these functions, are defined inside one function, cacheData retains
-		 return {
-          list: function (channelName, callback){
-		  console.log ('channelName='+channelName);
+        }
+    };
+});
+
+menuApp.factory('dataService', function($http) {
+	return {
+		sendMessage: function(message, channel, user, success, failure) {
             $http({
-              method: 'GET',
-              url:    'http://localhost:8080/channelChats/'+channelName,
-              cache: true
+                method: 'POST',
+                url: '/message/',
+				data: {message: message, channel: channel, user: user}
+            }).then(success, failure);
+        }
+	};
+});
+
+
+
+menuApp.controller('MenuCtrl', function($scope, channels, $routeParams) {
+    count = count + 1;
+    console.log(count + ', in getMenu function, userName=' + $routeParams.userName);
+    if (myChannelList.length === 0) {
+        channels.list($routeParams.userName, function(channels) {
+            $scope.channels = channels;
+            console.log("***$scope.channels, " + $scope.channels);
+            myChannelList = $scope.channels;
+            //  $scope.subviewChannelChats= "channelChats.html";
+        });
+    }
+
+});
+
+
+menuApp.factory('channelChats', function($http) {
+
+    //  if ( ! (typeof $routeParams.channelName === "undefined") ) {
+    // var cachedData;
+    //these functions, are defined inside one function, cacheData retains
+    return {
+        list: function(channelName, callback) {
+            console.log('channelName=' + channelName);
+            $http({
+                method: 'GET',
+                url: '/channelChats/' + channelName,
+                cache: true
             }).success(callback);
-          }
-        };
-      });
+        }
+    };
+});
 
-		getChannelChat = function ($scope, channelChats, $routeParams, $rootScope){
-		count = count+1;
-			  console.log ( count + ', in ChatMessageCtrl, ' + $routeParams.channelName);
-			  $scope.displayName = $routeParams.channelName;
-			  /*
-			  if ( ($routeParams.channelName).length > 0) {
-				$scope.msgBoxshowBL = true;
-			  }
-			  else
-				  $scope.msgBoxshowBL = false;
-			  */
-			  console.log ('in ChatMessageCtrl, myChannelList=' + myChannelList);
-		
-				channelChats.list($routeParams.channelName, function(channelChats) {
-					  $scope.channelChats = channelChats;
-					  $scope.channels = myChannelList;
-					  
-					});
 
-				};
-				
-      menuApp.controller('ChatMessageCtrl', getChannelChat );
-		  
-    // menuApp.controller('AllCtrl', getMenu,getChannelChat );
-	 /*
+menuApp.controller('ChatMessageCtrl', function($scope, channelChats, $routeParams, $rootScope, dataService) {
+    count = count + 1;
+    console.log(count + ', in ChatMessageCtrl, ' + $routeParams.channelName);
+    $scope.displayName = $routeParams.channelName;
+    
+    console.log('in ChatMessageCtrl, myChannelList=' + myChannelList);
+
+    channelChats.list($routeParams.channelName, function(channelChats) {
+        $scope.channelChats = channelChats;
+        $scope.channels = myChannelList;
+
+    });
+
+    $scope.sendMessage = function() {
+		console.log('message to add', $scope.msg);
+		var user = 'john'; // TODO temp hardcoded!
+		var msg = $scope.msg;
+		dataService.sendMessage($scope.msg, $routeParams.channelName, user, 
+			(response) => {
+				var newMessage = {SENDER: user, DATE: new Date().toString(), MESSAGE: msg};
+				$scope.channelChats.push(newMessage);
+			},
+			(error) => {
+				console.log("Error: server failed to add message.");
+			}
+		);
+		$scope.msg = '';
+    };
+
+});
+
+// menuApp.controller('AllCtrl', getMenu,getChannelChat );
+/*
 	 app.controller('SaveChannelMsgCtrl', function($scope) {
 		 count = count+1;
 		 console.log ( count + ', in SaveChannelMsgCtrl, ');
@@ -105,4 +123,3 @@
     }
 });
 */
-	
