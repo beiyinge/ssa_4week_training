3cc3a65bc7b511989299f5d31f170ec1cb3f62f2
toniali <li_xuemei@hotmail.com>
2016-08-19 10:56:02 -0400

    added search

---
+++ day14/searchMsgUser.js
@@ -0,0 +1,43 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+	//function channelChatJSONPromise(db, channelName) {
+exports.searchChannelMsgJSONPromisePublic = function (db, searchKeyword){
+		var query = "SELECT CHANNELNAME, SENDER , MESSAGE , DATE "+
+		//	var query = "SELECT CHANNEL.CHANNELNAME "+
+		" from CHANNEL_CHAT where MESSAGE like '%" + searchKeyword +"%'" ;
+		
+		 console.log ('in searchChannelMsgJSONPromisePublic query=' +query);
+	    var channels = [];
+	    return new Promise((resolve, reject) => {
+	        db.serialize(function() {
+	            db.each(
+	                query, 
+	                function(err, row) {
+	                	if (err){
+	                		reject(err);
+	                	}
+	                	else{
+	                		//channels.push(row.CHANNELNAME);
+	                		channels.push(row);
+	                	}
+	                },
+	                function (err) {
+	                	if (err){
+	                		reject(err);
+	                	}
+	                	else{
+	                		resolve (JSON.stringify(channels));	
+	                		//resolve (channels);
+	                	}
+	                    
+	                }
+	            );
+	        });
+
+	    });
+	    
+	  
+	}
+	
