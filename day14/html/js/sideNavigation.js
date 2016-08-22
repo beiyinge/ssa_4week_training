b30a2ba9e28e1c1cd3f10bf24944cae103a09731
toniali <li_xuemei@hotmail.com>
2016-08-22 12:50:39 -0400

    updated search nav

--- day14/html/js/sideNavigation.js
+++ day14/html/js/sideNavigation.js
@@ -10,12 +10,14 @@ function closeRightNav() {
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openSearchNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+	console.log ("in openSearchNav");
+    document.getElementById("mySearchSidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeSearchNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySearchSidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
 }
+
