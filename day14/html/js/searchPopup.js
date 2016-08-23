a8518287d0b686a551905bac40350266e9491dd3
toniali <li_xuemei@hotmail.com>
2016-08-23 09:22:16 -0400

    cleaned up

--- day14/html/js/searchPopup.js
+++ day14/html/js/searchPopup.js
@@ -63,23 +63,6 @@
 				};
 				
       
-	  
-	  menuApp.service('searchResultsServiceNew', function() {
-		  var productList = [];
 
-		  var addProduct = function(newObj) {
-			  productList.push(newObj);
-		  };
-
-		  var getProducts = function(){
-			  return productList;
-		  };
-
-		  return {
-			addProduct: addProduct,
-			getProducts: getProducts
-		  };
-
-		});
 
 
