c7acdf857771ab6c91bb380435053ea528850fc6
toniali <li_xuemei@hotmail.com>
2016-08-24 10:22:41 -0400

    added user icon to all.html, and added a new column created_user on table team.

--- day14/SlackDB.js
+++ day14/SlackDB.js
@@ -32,6 +32,7 @@ function createSlackTables(db){
 	        var createTeamTableSql = "CREATE TABLE IF NOT EXISTS TEAM " +
 	                    "(TEAMNAME        CHAR(25)    NOT NULL," +
 	                    " DESCR         CHAR(140)  , " + 
+						" CREATED_USER         CHAR(140)  , " + 
 	                    " DATE          TEXT        NOT NULL)"; 
 	        
 	        var createTeamUserTableSql = "CREATE TABLE IF NOT EXISTS TEAMUSER " +
@@ -77,43 +78,82 @@ function insertDummyData (db){
     var insertUserSql = "INSERT INTO USER (USERNAME, FIRSTNAME,LASTNAME, PASSWORD," +
 	"EMAIL, DATE) " +
 "VALUES ('shuvo','Shuvo', 'Ahmed','shuvopassword','shuvo@ssa.org','2016-08-05 12:45:00')," +
-       "('john',     'john','Moinuddin',    'johnpassword','john@ssa.org','2016-08-05 12:45:00')," +
+       "('josephine',     'josephine','benedict',    'josepassword','john@ssa.org','2016-08-05 12:45:00')," +
        "('charles','Charles','Walsek',   'charlespassword','3@ssa.org','2016-08-05 12:45:00')," +
        "('beiying','Beiying','Chen',     'beiyingpassword','5@ssa.org','2016-08-05 12:45:00')," +
-       "('swarup',  'Swarup','Khatri',    'swarup','6@ssa.org','2016-08-05 12:45:00');"; 
+       "('xuemei',  'Xuemei','Li',    'xuemeipassword','6@ssa.org','2016-08-05 12:45:00');"; 
 db.run(insertUserSql); 
-
-var insertTeamSql = "INSERT INTO TEAM (TEAMNAME, DESCR, DATE) " +
-	"VALUES ('Wonder_Team','Wonderful Team','2016-08-05 12:45:00');"; 
+	        console.log ('Done, insertUserSql='+insertUserSql);
+var insertTeamSql = "INSERT INTO TEAM (TEAMNAME, DESCR, CREATED_USER, DATE) " +
+	"VALUES ('Wonder_Team','Wonderful Team', 'beiying', '2016-08-05 12:45:00')," +
+	"('Wonder_Team1','Wonderful Team 1','beiying', '2016-08-05 12:45:00');";
 db.run(insertTeamSql);
-    
+    console.log ('Done, insertTeamSql='+insertTeamSql);
+	
 var insertTeamUserSql = "INSERT INTO TEAMUSER (TEAMNAME, USERNAME, DESCR, DATE) " +
  "VALUES ('Wonder_Team','shuvo',   'empty ','2016-08-05 12:45:00'), " +
-        "('Wonder_Team','beiying',   'john in the team ','2016-08-05 12:45:00'), "+
-"('Wonder_Team1','john',   'john in the team ','2016-08-05 12:45:00');";
-"('Wonder_Team1','xuemei',   'john in the team ','2016-08-05 12:45:00');";
+        "('Wonder_Team','beiying',   'join the team ','2016-08-05 12:45:00'), "+
+		  "('Wonder_Team','josephine',   'join the team ','2016-08-05 12:45:00'), "+
+    "('Wonder_Team','charles',   'join the team ','2016-08-05 12:45:00'), " +
+    "('Wonder_Team','xuemei',   'join the team ','2016-08-05 12:45:00'), " + 
+		  "('Wonder_Team1','charles',   'join the team ','2016-08-05 12:45:00'), " +
+"('Wonder_Team1','xuemei',   'join the team ','2016-08-05 12:45:00');";
 
 db.run(insertTeamUserSql);
+console.log ('Done, insertTeamUserSql='+insertTeamUserSql);
 
 var insertChannelTableSql = "INSERT INTO CHANNEL (CHANNELNAME, TEAMNAME, PUBLIC_FLAG, DATE) " +
-"VALUES ('B_channel','Wonder_Team',   'Y','2016-08-05 12:45:00'), " +
+"VALUES ('A_channel','Wonder_Team',   'Y','2016-08-05 12:45:00'), " +
 " ('C_channel','Wonder_Team',   'Y','2016-08-05 12:45:00'), " +
 " ('D_channel','Wonder_Team1',   'Y','2016-08-05 12:45:00'), " +
-"('A_channel',    'Wonder_Team1',   'N','2016-08-05 12:45:00');";
+"('E_channel',    'Wonder_Team1',   'N','2016-08-05 12:45:00');";
 
 db.run(insertChannelTableSql);
-//console.log("insertChannelTableSql="+insertChannelTableSql);
+console.log ('Done, insertChannelTableSql='+insertChannelTableSql);
 
 var insertChannelChatSql =  "INSERT INTO  CHANNEL_CHAT (CHANNELNAME, SENDER , MESSAGE , DATE ) "+
-" values "+
-"( 'A_channel', 'john', 'Hello 1', '2016-08-05 12:45:00'), " +
-"( 'A_channel', 'xuemei', 'Hello 2', '2016-08-05 12:45:00'), " +
- "( 'A_channel', 'beiying', 'Hello 3', '2016-08-05 12:45:00'); "
+" values " +
+"( 'A_channel', 'charles', 'am working on saving messages.', '2016-08-05 12:45:00'), " +
+"( 'A_channel', 'xuemei', 'We are planing to use js-beatutify to format files.', '2016-08-05 12:45:00'), " +
+ "('A_channel','xuemei','Let''s use bootstrap to make UI pretty','2016-08-05 12:45:00'), " +
+ "('A_channel','beiying','I am working on creating team.','2016-08-05 12:45:00'), " +
+ "('A_channel','josephine','I am working mocha test scripts.','2016-08-19 14:08:08'), " +
+ "('A_channel','xuemei','Thank you for taking care this josephine.','2016-08-19 14:10:08'), " +
+ "('A_channel','charles','New operator seperation is very important.','2016-08-19 14:14:53'), " +
+ "('C_channel','josephine','hello','2016-08-19 14:15:12'), " +
+ "('A_channel','john','dependance Injection.','2016-08-19 14:22:54'), " +
+ "('C_channel','charles','infrastralized data management infrastructure automation','2016-08-19 14:23:04'), " +
+ "('A_channel','josephine','angularjs unit testing','2016-08-19 14:33:32'), " +
+ "('C_channel','charles','Microservice is a subset of  SOA','2016-08-19 14:33:41'), " +
+ "('A_channel','john','OMG','2016-08-19 14:56:22'), " +
+ "('D_channel','charles','I am working on styles.','2016-08-19 14:56:37'), " +
+ "('A_channel','josephine','I am working on uploading a file.','2016-08-19 14:58:52'), " +
+ "('D_channel','john','Rapid application deployment.','2016-08-19 14:59:15'), " +
+ "('A_channel','charles','multiple platforms','2016-08-19 15:28:07'), " +
+ "('C_channel','josephine',' push out imagedocker tag nodejs-microservices your_dockerhub_user nodejs-microservices','2016-08-19 15:29:13'), " +
+ "('A_channel','charles','test individual classes in isolation','2016-08-19 15:38:27'), " +
+ "( 'A_channel', 'beiying', 'I agreed.', '2016-08-05 12:45:00'); "
 
 db.run(insertChannelChatSql);
-//db.each("SELECT CHANNELNAME, TEAMNAME FROM CHANNEL", function(err, row) {
-//console.log("row.CHANNELNAME: " + row.CHANNELNAME + " TEAMNAME="+TEAMNAME);
-//});
+
+console.log ('Done, insertChannelChatSql='+insertChannelChatSql);
+
+var insertDirectChatSql = "INSERT INTO DIRECT_CHAT (SENDER,RECIEVER,MESSAGE,DATE) "+
+ " VALUES " +
+ " ('xuemei','beiying','github is cool','2016-08-02 12:45:00'), " +
+ " ('beiying','xuemei','learn git','2016-08-02 12:40:00'), " +
+ " ('josephine','xuemei','git clone','2016-08-02 12:48:00'), " +
+ " ('xuemei','josephine','I just pushed latest code, please pull.','2016-08-02 12:50:00'), " +
+ " ('charles','beiying','we are learning Angular JS.','2016-08-22 10:10:36'), " +
+ " ('beiying','charles','We can try to isntall docker toolbox on our windows 7','2016-08-22 10:10:47'), " +
+ " ('charles','beiying','Just tried, docker works now on my laptop.','2016-08-22 13:15:38'); "
+ 
+ db.run(insertDirectChatSql);
+ 
+  console.log ('*****Done, insertDirectChatSql='+insertDirectChatSql);
+  db.each("SELECT CHANNELNAME, TEAMNAME FROM CHANNEL", function(err, row) {
+  console.log("row.CHANNELNAME: " + row.CHANNELNAME + " TEAMNAME="+TEAMNAME);
+  });
 }
 
 exports.insertTeam = function  (db, teamName, descr){
@@ -345,12 +385,12 @@ exports.channelJSONPromisePublic = function (db, username){
   
 }
 	
-	//function channelChatJSONPromise(db, channelName) {
+//	function channelChatJSONPromise(db, channelName) {
 exports.channelChatJSONPromisePublic = function (db, channelName){
 		var query = "SELECT CHANNELNAME, SENDER , MESSAGE , DATE "+
 		//	var query = "SELECT CHANNEL.CHANNELNAME "+
 		" from CHANNEL_CHAT where CHANNELNAME = '" + channelName +"'" ;
-		 console.log ('in channelChatJSONPromise ;')
+		 console.log ('in channelChatJSONPromise, ')
 	    var channels = [];
 	    return new Promise((resolve, reject) => {
 	        db.serialize(function() {
@@ -456,7 +496,7 @@ exports.channelChatJSONPromisePublic = function (db, channelName){
 	
 	function getChannelChat (db, channelName) {
 		//var data = function () {
-		console.log('in channel_chat, ');
+		console.log('in channel_chat, ' + channelName);
 			return channelChatJSONPromise(db,channelName).then(
 					(val) =>{
 						
@@ -479,8 +519,7 @@ exports.channelChatJSONPromisePublic = function (db, channelName){
 			
 
 
-	//createSlackTables(db);
-	//insertDummyData (db);
+//createSlackTables(db);
+//insertDummyData (db);
 	
-//getChannels2 (db, 'john');
-//getChannelChat (db, 'A_channel');
+
